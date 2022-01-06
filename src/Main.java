b587f850e302c814d4cfbecbb038a785e8513773
Andrey <azenin010@gmail.com>
2022-01-06 17:57:06 +0400

    Мили commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        int ticket = 400;
+        int mile = 1;
+        int money = 20;
+        int bonusMile = ticket/money;
+            System.out.println("Бонусные мили :" + bonusMile);
+
+
+    }
+}
